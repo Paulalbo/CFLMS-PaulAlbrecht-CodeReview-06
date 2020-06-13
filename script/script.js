97f57f92a9ef45149a0f1c4f20d9936cec7ec70e
Paulalbo <65129276+Paulalbo@users.noreply.github.com>
2020-06-13 14:56:28 +0200

    js fixes

--- script/script.js
+++ script/script.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
     for (serie of series) {
 
         getimggrid.innerHTML +=
-        `<div id="${serie.id}" title="${serie.title}" image="${serie.image}" description="${serie.description}" class="seriesimg"><img src=\"${serie.image}\"></div>`;
+        `<div id="${serie.id}2" title="${serie.title}" image="${serie.image}" description="${serie.description}" class="seriesimg"><img src=\"${serie.image}\"></div>`;
         
         getcarousel.innerHTML +=
-        `<div id="${serie.id}2" title="${serie.title}" image="${serie.image_m}" description="${serie.description}" class="seriesimgm"><span>&hearts; 0</span><img src=\"${serie.image_m}\"></div>`;
+        `<div id="${serie.id}" title="${serie.title}" image="${serie.image_m}" description="${serie.description}" class="seriesimgm${serie.id}"><span id="${serie.id}">❤ </span><img src=\"${serie.image_m}\"></div>`;
 
-        // show only 5 images in carousel div
+        // show only the first 5 images in carousel div
         $(".carousel > div").slice(5).remove();
     }
 
@@ -48,27 +48,58 @@ $(".seriesimg").on("click", function() {
 });
 
 
+    
 
     //click counter
-
-
-    $(".seriesimgm").on("click", function() { 
-
-        let title = $(this).attr("title");
-        let image = $(this).attr("image");
-        let description = $(this).attr("description");
-        let id = $(this).attr("id");
-
     
-            let count = 0;
-            $(this).onclick = function() {
-            count += 1;
-            $("span").append(`&hearts; ${count}`);
-            }
-          });
+    counter1 = 0
+    counter2 = 0
+    counter3 = 0
+    counter4 = 0
+    counter5 = 0
         
-           
-          
+    $(".seriesimgm1").click(function() { 
+        counter1++;
+
+            $("#1 span").text("❤ " + counter1);
+        });
+
+    $(".seriesimgm2").click(function() { 
+        counter2++;
+
+            $("#2 span").text("❤ " + counter2);
+        });
+    
+    $(".seriesimgm3").click(function() { 
+        counter3++;
+
+            $("#3 span").text("❤ " + counter3);
+        });
+
+    $(".seriesimgm4").click(function() { 
+        counter4++;
+
+            $("#4 span").text("❤ " + counter4);
+        });
+
+    $(".seriesimgm5").click(function() { 
+        counter5++;
+
+            $("#5 span").text("❤ " + counter5);
+        });
+
+
+
+        // sort function 
+        $(".sort button").click(function() { 
+            var movielist1 = $(".seriesimgm1");
+            var movielist2 = $(".seriesimgm2");
+            var movielist3 = $(".seriesimgm3");
+            var movielist4 = $(".seriesimgm4");
+            var movielist5 = $(".seriesimgm5");
+            
+
+        });
 
 });
 
